977d03067212df5e36ee8407ffd6429abe23bbb2
Sportman9x9 <48306527+Sportman9x9@users.noreply.github.com>
2020-11-12 23:03:15 -0600

    Add files via upload

---
+++ ayuda.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "***************************************\n"
+echo -e "*** Has accedido a la ayuda ***********\n"
+echo -e "***************************************\n"
+
+echo -e "-Arbol:  muestra la jerarquía de un directorio\n"
+echo -e "-Ayuda: Da información acerca de los comandos\n"
+echo -e "-infosys: Te da las características del sistema actual\n"
+echo -e "-Fecha: Te da la fecha actual\n"
+echo -e "-Hora: Te da la hora actual\n"
+echo -e "-Buscar: Buscará un archivo, recibe como argumento el archivo a buscar y el directorio a buscar\n"
+echo -e "-Créditos: Muestra los créditos de los desarrolladores \n "
+echo -e "Salir: Saldrás del programa \n"
+echo -e "Juego 1: Se iniciará el juego 1 \n"
+echo -e "Juego 2: Se iniciará el juego 2 \ns"
+
+echo -e "Este programa ha sido creado con fines académicos, publico ageno a cualquier partido político
+queda prohibido su uso para fines distintos a los establecidos en el programa. \n"
