b250202dbe266b44489c365044d050c562ee1ada
Athenas-Marlene-GM <74329545+Athenas-Marlene-GM@users.noreply.github.com>
2020-11-13 12:49:13 -0600

    Update arbol.sh

--- arbol.sh
+++ arbol.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 esc=$(printf '\033')
 #B=$(tput setaf 4)
-#N=$(tput op)
-read directorio
+#N=$(tput op) 
+read -p "Dame el directorio o presiona enter para mostrar el arbol del directorio actual: " -e directorio
 if [ "$directorio" == "" ] 
 then
 		find $PWD |sed -e 's;[^/]*/;|____;g;s;____|;  |;g' 
